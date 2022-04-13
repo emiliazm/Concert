328f97d2528a7072babfe043bab30b97f1027ddb
emiliazm <98142626+emiliazm@users.noreply.github.com>
2022-04-13 22:27:35 +0200

    Modify bands section styles and erase the section on html.

--- script.js
+++ script.js
@@ -14,55 +14,55 @@ document.querySelectorAll('.mobile-nav-list-container').forEach((n) => n.addEven
 
 const bands = [
   {
-    class: 'band1',
-    image: './images/m-mileyccyrus.jpg',
+    class: 'singer',
+    image: './images/m-mileyccyrus.webp',
     name: 'Miley Cyrus',
     description1: 'American singer, songwriter, actress, and television personality.',
     description2: 'Known for her distinctive raspy voice, her music incorporates elements of varied styles and genres, including pop, country pop, hip hop, experimental, and rock.',
   },
 
   {
-    class: 'band2',
-    image: './images/m-dojacat.jpg',
+    class: 'singer',
+    image: './images/m-dojacat.webp',
     name: 'Doja Cat',
     description1: 'American rapper, singer, songwriter, and record producer.',
     description2: 'Born and raised in Los Angeles, she began making and releasing music on SoundCloud as a teenager.',
   },
 
   {
-    class: 'band2',
-    image: './images/m-dojacat.jpg',
+    class: 'singer',
+    image: './images/m-camilo.jpg',
     name: 'Camilo Echeverry',
     description1: 'Colombian singer, musician and songwriter.',
     description2: 'His music is generally categorized as Latin pop with a mix of urbano and is noted for his romantic lyrics and soprano voice.',
   },
 
   {
-    class: 'band2',
-    image: './images/m-guns.jpg',
+    class: 'singer',
+    image: './images/m-guns.webp',
     name: 'Guns N Roses',
     description1: 'American hard rock band from Los Angeles, California, formed in 1985.',
-    description2: 'Guns N Roses debut album, Appetite for Destruction (1987), reached number one on the Billboard 200 a year after its release, on the strength of the top 10 singles "Welcome to the Jungle", "Paradise City", and "Sweet Child o Mine",',
+    description2: 'Guns N Roses debut album, Appetite for Destruction (1987), reached number one on the Billboard 200 a year after its release.',
   },
 
   {
-    class: 'band2',
-    image: './images/m-taylorswift.jpg',
+    class: 'singer',
+    image: './images/m-taylorswift.webp',
     name: 'Tylor Swift',
     description1: 'American singer-songwriter.',
     description2: 'Born and raised in Los Angeles, she began making and releasing music on SoundCloud as a teenager.',
   },
 
   {
-    class: 'band2',
-    image: './images/m-dojacat.jpg',
+    class: 'singer',
+    image: './images/m-mariabecerra.jpg',
     name: 'Maria Becerra',
     description1: 'Argentinian singer and former YouTuber.',
     description2: 'She has been dubbed as "a leading voice in Argentina\'s urban pop movement".',
   },
 ];
 
-const title = document.querySelector('.bands .bands-h2');
+const title = document.querySelector('#bands .bands-h2');
 let inDiv = '';
 
 bands.forEach((singer) => {
@@ -73,11 +73,12 @@ bands.forEach((singer) => {
       <h4 class="info-h4">"${singer.description1}"</h4>
       <p class="info-p">"${singer.description2}"</p>
     </div>
-  </div>
+    </div>
   `;
 });
 
 const divToBands = document.createElement('div');
+divToBands.setAttribute('class', 'band-grid');
 divToBands.innerHTML = inDiv;
 
 title.after(divToBands);
